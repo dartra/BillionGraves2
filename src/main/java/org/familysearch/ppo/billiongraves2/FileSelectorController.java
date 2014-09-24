b4b9b3e9631fb8720eaccdad612f3fc0f5d6e748
Richard Dart <dartra@familysearch.org>
2014-09-24 10:57:14 -0600

    latestToDesktop

--- src/main/java/org/familysearch/ppo/billiongraves2/FileSelectorController.java
+++ src/main/java/org/familysearch/ppo/billiongraves2/FileSelectorController.java
@@ -7,17 +7,13 @@ package org.familysearch.ppo.billiongraves2;
 
 import java.io.File;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.util.Callback;
 import org.familysearch.ppo.javafx.ListCellController;
-import org.familysearch.ppo.javafx.ListCellControllerAware;
 import org.familysearch.ppo.javafx.SmartController;
 
 /**
@@ -25,7 +21,7 @@ import org.familysearch.ppo.javafx.SmartController;
  *
  * @author DartRA
  */
-public class FileSelectorController extends SmartController implements ListCellControllerAware<File> {
+public class FileSelectorController extends SmartController{
 
     @FXML
     private ListView<File> lstViewFiles;
@@ -34,23 +30,13 @@ public class FileSelectorController extends SmartController implements ListCellC
 
     @Override
     public void init(URL url, ResourceBundle rb) {
-        getAppHelper().setListCellFactoryFxmlFor(lstViewFiles, "/fxml/FileEntryLine.fxml", this);
+        this.useFxmlForListView(lstViewFiles, "/fxml/FileEntryLine.fxml");
+//        getAppHelper().setListCellFactoryFxmlFor(lstViewFiles, "/fxml/FileEntryLine.fxml", this);
         files = lstViewFiles.getItems();
     }
 
     @FXML
     void selectFilesAction(ActionEvent event) {
          
-    }
-
-    @Override
-    public void controllerWasAdded(ListCellController<File> controller) {
-        FileEntryLineController c = (FileEntryLineController) controller;
-        c.onRemove(new Callback<File, Void>() {
-            @Override public Void call(File file) {
-                files.remove(file);
-                return null;
-            }
-        });
     }
 }
